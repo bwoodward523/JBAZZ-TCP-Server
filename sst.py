40f5635a46709f335736fb921f9d1513389a1fe1
bwoodward523 <bwoodward@ycp.edu>
2026-02-15 13:37:38 -0500

    Speech to text model

--- sst.py
+++ sst.py
@@ -1,8 +1,11 @@
 import io
 
 def convert_to_text(payload, whisper_model):
+
+   
     print("Implement convert to text")
     #Convert the bytes into an io buffer
+
     buffer = io.BytesIO(payload)
 
     #Name the buffer as a file so whisper accepts it
@@ -10,6 +13,7 @@ def convert_to_text(payload, whisper_model):
 
     print("Audio received, transcribing...")
 
+    assert whisper_model != None
     #Sending the buffer to whisper.
     segments, info = whisper_model.transcribe(buffer, language="en")
 
@@ -21,15 +25,22 @@ def convert_to_text(payload, whisper_model):
 def test_audio():
     from faster_whisper import WhisperModel
 
-    whisper_model = WhisperModel("base", device="cpu", compute_type="float16")
+    whisper_model = WhisperModel("base", device="cuda", compute_type="float16")
 
 
-    with open("test.mp3", "rb") as f:
+    with open("output.wav", "rb") as f:
         audio = f.read()
-    
+        print(audio)
     transcript = convert_to_text(audio, whisper_model)
     print(transcript)
 
-
+def save_debug_wav():
+    import wave
+    wf = wave.open("output.wav", 'wb')
+    wf.setnchannels(self.channels)
+    wf.setsampwidth(self.p.get_sample_size(self.sample_format))
+    wf.setframerate(self.fs)
+    wf.writeframes(bytes)
+    wf.close()
 if __name__ == "__main__":
     test_audio()
